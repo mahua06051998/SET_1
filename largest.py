308ae711118bb6d1d2eed778ec53ba582a79789c
Mahua <31098356+mahua06051998@users.noreply.github.com>
2019-04-19 01:00:25 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,9 +1,9 @@
-num1=input()
-num2=input()
-num3=input()
-if (num1>num2) and (num1>num3):
-       print(num1)
-if (num2>num1) and (num2>num3):
-       print(num2)
+no1 = input()
+no2 = input()
+no3 = input()
+if (no1 > no2) and (no1 > no3):
+       print(no1)
+if (no2 > no1) and (no2 > no3):
+       print(no2)
 else:
-       print(num3)
+       print(no3)
