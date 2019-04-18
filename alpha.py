8ad29dd7edeb49e5c90aeca23eeb0c126c9c0d95
Mahua <31098356+mahua06051998@users.noreply.github.com>
2019-04-19 00:57:01 +0530

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -1,5 +1,5 @@
-ch=input()
-if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
+c = input()
+if((c >= 'a' and c <= 'z') or (c >= 'A' and c <= 'Z')):
        print("Alphabet")
 else:
        print("No")
