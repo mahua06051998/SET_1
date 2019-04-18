2f23ca7edcf5f15e7cc4b553338be0b95e8c434c
Mahua <31098356+mahua06051998@users.noreply.github.com>
2019-04-19 00:54:47 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,6 +1,6 @@
-ch=input()
-if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
-       if ch in ('a', 'e' ,'i' ,'o' , 'u'):
+c =input()
+if((c >= 'a' and c <= 'z') or (c >= 'A' and c <= 'Z')):
+       if c in ('a', 'e' ,'i' ,'o' , 'u'):
               print("Vowel")
        else:
                print("Consonant")
